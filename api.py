39411f172f66cf7e0159a7798c42f399ddbb97aa
Rafno <rafnar1993@gmail.com>
2019-02-02 15:45:02 +0000

    smá API vinna í gangi

--- api.py
+++ api.py
@@ -1,68 +1,65 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
+with open('description.json') as f:
+    data = json.load(f)
+
 
 #Function used to send error if id does not exist
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+def abort_if_image_doesnt_exist(image_id):
+    if image_id not in data:
+        abort(404, message="Todo {} doesn't exist".format(image_id))
 
 #init parser
 parser = reqparse.RequestParser()
 
 #Makes it a must to include a task param in the request
-parser.add_argument('task')
+#parser.add_argument('task')
 
 
 # Todo
 # shows a single todo item and lets you delete a todo item
 class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+    def get(self, image_id):
+        abort_if_image_doesnt_exist(image_id)
+        return data[image_id]
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
+    def delete(self, image_id):
+        abort_if_image_doesnt_exist(image_id)
+        del data[image_id]
         return '', 204
 
-    def put(self, todo_id):
+    def put(self, image_id):
         args = parser.parse_args()
         task = {'task': args['task']}
-        TODOS[todo_id] = task
+        data[image_id] = task
         return task, 201
 
 
 # TodoList
 # shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
+class ImageList(Resource):
     def get(self):
-        return TODOS
+        return dataTodo
 
     def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        image_id = int(max(data.keys()).lstrip('todo')) + 1
+        image_id = 'todo%i' % image_id
+        data[image_id] = {'task': args['task']}
+        return data[image_id], 201
+
+
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
-
+api.add_resource(ImageList, '/')
+#api.add_resource(Todo, '/todos/<todo_id>')
 if __name__ == '__main__':
     #app.run(debug=True)
     app.run()
